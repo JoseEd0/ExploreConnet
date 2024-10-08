f21ee18b92b2421dac927a43d04448d2b056e4a9
eliseocervantes <jireh.cervantes@utec.edu.pe>
2024-10-07 21:24:06 -0500

    avance de foro 1

--- src/main/java/dbp/exploreconnet/post/infrastructure/PostRepository.java
+++ src/main/java/dbp/exploreconnet/post/infrastructure/PostRepository.java
@@ -1,23 +1,3 @@
 package dbp.exploreconnet.post.infrastructure;
 
 
-import dbp.exploreconnet.post.domain.Post;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findBySongId(Long songId);
-
-    List<Post> findByAlbumId(Long albumId);
-
-    Page<Post> findByUserId(Long userId, Pageable pageable);
-
-    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
-
-    Page<Post> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
-}
